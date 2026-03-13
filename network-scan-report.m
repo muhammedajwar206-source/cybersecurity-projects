993b6f7249309b653a56396425e4b90ca04e5e95
muhammedajwar206-source <muhammedajwar206@gmail.com>
2026-03-13 12:16:07 +0530

    Create network-scan-report.md

---
+++ network-scan-report.m
@@ -0,0 +1,17 @@
+# Network Scan Report
+
+Tools Used:  Nmap
+
+Objective:
+To identify open ports and services in the local network.
+
+Command Used:
+nmap -sV 192.168.1.1
+
+Findings:
+- Port 22 : SSH
+- Port 80 : HTTP
+- Port 443 : HTTPS
+
+Conclusion:
+Basic network scanning helps identify exposed services
